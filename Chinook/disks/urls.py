b3e520cb7ec72baa1a6bc50fb9e48fc57d5a4f96
JBortolussi <julien.borto@orange.fr>
2020-04-25 19:22:43 +0200

    site fini (normalement)

--- Chinook/disks/urls.py
+++ Chinook/disks/urls.py
@@ -1,4 +1,4 @@
-"""Chinook URL Configuration
+"""Disks URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from disks.views import home
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home),
-    path('disks/', include('disks.urls')),
+    path('', views.home),
+    path('<int:album_id>', views.home, name="search"),
+    path('<int:album_id>/<str:titre>', views.home_search_select, name="search_select"),
 ]
--- Chinook/disks/views.py
+++ Chinook/disks/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from disks.models import Album, Artist, Track
+from django.http import Http404
 # Create your views here.
+
+def home(request, album_id=1):
+    if ("titre" in request.GET):
+        titre = request.GET['titre']
+        albums = Album.objects.using('chinook').filter(title__contains=titre)
+        if (not albums.filter(id=album_id)): #on test si l'article selectionné est dans la recherche
+            if (albums):
+                album_display = albums[0]
+                tracks = Track.objects.using('chinook').filter(album=album_display)
+            else:
+                titre=''
+                album_display = None
+            return render(request, "disks/home.html", locals())
+    else:
+        titre = ''
+        albums = Album.objects.using('chinook').all()
+
+    if (album_id != None):
+        try:
+            album_display=albums.get(id=album_id)
+            tracks = Track.objects.using('chinook').filter(album=album_display)
+        except Album.DoesNotExist:
+            raise Http404
+    else:
+        album_display=None
+
+    return render(request, "disks/home.html", locals())
+
+def home_search_select(request, album_id, titre):
+    albums = Album.objects.using('chinook').filter(title__contains=titre)
+    if (not albums.filter(id=album_id)):  # on test si l'article selectionné est dans la recherche
+        album_display = None
+        return render(request, "disks/home.html", locals())
+
+    try:
+        album_display = albums.get(id=album_id)
+        tracks = Track.objects.using('chinook').filter(album=album_display)
+    except Album.DoesNotExist:
+        raise Http404
+
+    return render(request, "disks/home.html", locals())
